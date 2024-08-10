24df0a9664a6df7c536fa2c1ad117067fddb9cca
AdepojuWasiu <adepojuwasiu031@gmail.com>
2024-08-10 22:47:50 +0100

    fix typo

--- app/api/bot/route.js
+++ app/api/bot/route.js
@@ -1,20 +1,51 @@
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 
-export const fetchCache = 'force-no-store'
+// export const fetchCache = 'force-no-store'
 
-import { Bot, webhookCallback } from 'grammy'
+// import { Bot, webhookCallback } from 'grammy'
+
+// const token = process.env.TELEGRAM_BOT_TOKEN
+
+// if (!token) throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.')
+
+// bot.command('start', async (ctx) => {
+//     await ctx.reply('Welcome')
+// })
+
+// const bot = new Bot(token)
+// bot.on('message:text', async (ctx) => {
+//   await ctx.reply(ctx.message.text)
+// })
+
+// export const POST = webhookCallback(bot, 'std/http')
+
+import TelegramBot from 'node-telegram-bot-api';
 
 const token = process.env.TELEGRAM_BOT_TOKEN
+const bot = new TelegramBot(token);
 
-if (!token) throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.')
+export default function handler(req, res) {
+    if (req.method === 'POST') {
+        // Log the request body for debugging
+        console.log('Received a POST request:', req.body);
 
-bot.command('start', async (ctx) => {
-    await ctx.reply('Welcome')
+        // Process the incoming webhook update
+        bot.processUpdate(req.body);
+        res.status(200).json({ message: 'Webhook received' });
+    } else {
+        // Log the method used in the request
+        console.log(`Received a ${req.method} request. Responding with 405.`);
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const message = msg.text;
+    console.log(`Received message: ${message} from chatId: ${chatId}`);
+
+    if (message === '/start') {
+        bot.sendMessage(chatId, 'Welcome to the bot');
+    }
 })
-
-const bot = new Bot(token)
-bot.on('message:text', async (ctx) => {
-  await ctx.reply(ctx.message.text)
-})
-
-export const POST = webhookCallback(bot, 'std/http')
