fbfd37bcee216e7fbd4e013aa901b474ce3dc41a
AdepojuWasiu <adepojuwasiu031@gmail.com>
2024-08-11 15:18:13 +0100

    fix typo

--- app/eco-system/page.jsx
+++ app/eco-system/page.jsx
@@ -1,17 +1,17 @@
-// 'use client'
+'use client'
 
-// const EcoSystem = () => {
-//   return (
-//     <div className="w-full flex justify-center items-center flex-col 
-//            xl:mb-2 xl:mt-[10vw] mt-[20vw] gap-10 mb-[45vw]">
-//       <h1 className="text-[40px] text-[#c29b0c]">Coming Soon</h1>
-//       <p>The Ecosystem will be added at public launch</p>
+const EcoSystem = () => {
+  return (
+    <div className="w-full flex justify-center items-center flex-col 
+           xl:mb-2 xl:mt-[10vw] mt-[20vw] gap-10 mb-[45vw]">
+      <h1 className="text-[40px] text-[#c29b0c]">Coming Soon</h1>
+      <p>The Ecosystem will be added at public launch</p>
       
-//     </div>
-//   )
-// }
+    </div>
+  )
+}
 
-// export default EcoSystem
+export default EcoSystem
 
 
 
