09f9558ada068dffd555d0d8ead19f178d65dcf8
AdepojuWasiu <adepojuwasiu031@gmail.com>
2024-08-13 23:05:21 +0100

    fix typo

--- app/page.jsx
+++ app/page.jsx
@@ -181,7 +181,6 @@ const Home = () => {
                       top: `${click.y - 25}px`,
                       left: `${click.x - 15}px`,
                       animation: `slide-out-top ease forwards`,
-                      pointerEvents: 'none',
                     }}
                     onAnimationEnd={() => handleAnimationEnd(click.id)}
                   >
