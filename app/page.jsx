3a48344492aa03eb2d44df59a5b2ef991a92f798
AdepojuWasiu <adepojuwasiu031@gmail.com>
2024-08-13 09:12:58 +0100

    fix typo

--- app/page.jsx
+++ app/page.jsx
@@ -114,22 +114,22 @@ const Home = () => {
             <p className="pt-[10px] text-[30px] ml-[20px]" >{count}</p>
           </div>
         
-             <div className="flex justify-center justify-items-center w-full mt-[50px] h-[200px] relative" onClick={handleCardClick}>
-             {/* <Image src = '/assets/coin.jpeg' alt='logo' width= {300} height ={300}  /> */}
-             {clicks.map((click) => (
-              <div
-                key={click.id}
-                className="absolute text-10xl font-bold"
-                style={{
-                  top: `${click.y - 42}px`,
-                  left: `${click.x - 28}px`,
-                  animation: `float 1s ease-out`
-                }}
-                onAnimationEnd={() => handleAnimationEnd(click.id)}
-              >
-                11
-              </div>
-            ))}
+             <div className="flex justify-center justify-items-center w-full mt-[50px] relative" onClick={handleCardClick}>
+                    <Image src = '/assets/coin.jpeg' alt='logo' width= {300} height ={300}  />
+                    {clicks.map((click) => (
+                      <div
+                        key={click.id}
+                        className="absolute text-[20px] font-bold"
+                        style={{
+                          top: `${click.y - 42}px`,
+                          left: `${click.x - 28}px`,
+                          animation: `float 1s ease-out`
+                        }}
+                        onAnimationEnd={() => handleAnimationEnd(click.id)}
+                      >
+                        11
+                      </div>
+                  ))}
             </div>
              </div>
             )}
